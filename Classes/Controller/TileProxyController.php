7ee76ae8028395054b1a29752a99d0fc7083ce1f
Marc Mevert <marcmevert@gmail.com>
2026-01-18 15:44:03 +0100

    Refactor bounding box calculations in TileProxyController

--- Classes/Controller/TileProxyController.php
+++ Classes/Controller/TileProxyController.php
@@ -59,11 +59,15 @@ class TileProxyController extends ProxyController
         if (count($bbox) != 4) {
             return true;
         }
-        $minTileX = LatLngToTile::lngToTileX(intval($bbox[0]), $z);
-        $maxTileX = LatLngToTile::lngToTileX(intval($bbox[2]), $z);
-        $minTileY = LatLngToTile::latToTileY(intval($bbox[3]), $z);
-        $maxTileY = LatLngToTile::latToTileY(intval($bbox[1]), $z);
-        return !(!LatLngToTile::inRange($x, $minTileX, $maxTileX) || !LatLngToTile::inRange($y, $minTileY, $maxTileY));
+        [$west, $south, $east, $north] = array_map('floatval', $bbox);
+
+        $minTileX = min(LatLngToTile::lngToTileX($west, $z),LatLngToTile::lngToTileX($east, $z));
+        $maxTileX = max(LatLngToTile::lngToTileX($west, $z),LatLngToTile::lngToTileX($east, $z));
+        $minTileY = min(LatLngToTile::latToTileY($north, $z),LatLngToTile::latToTileY($south, $z));
+        $maxTileY = max(LatLngToTile::latToTileY($north, $z),LatLngToTile::latToTileY($south, $z));
+
+        return LatLngToTile::inRange((float)$x, $minTileX, $maxTileX)
+            && LatLngToTile::inRange((float)$y, $minTileY, $maxTileY);
     }
 
 
